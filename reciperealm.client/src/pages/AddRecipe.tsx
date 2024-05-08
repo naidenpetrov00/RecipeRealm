57fd10bb6445a6d92e7dbe7e99821fed78d62f32
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2024-05-08 21:38:42 +0300

    Finish AddRecipe ui

--- reciperealm.client/src/pages/AddRecipe.tsx
+++ reciperealm.client/src/pages/AddRecipe.tsx
@@ -1,6 +1,43 @@
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  Fragment,
+  MouseEventHandler,
+  useEffect,
+  useState,
+} from "react";
+
 import styles from "./Common/Form.module.css";
 
 const AddRecipe = () => {
+  const [recipeImages, setRecipeImages] = useState<string[]>([]);
+  const [showRemovePhoto, setShowRemovePhoto] = useState<boolean>(false);
+
+  const onMouseOverHandler: MouseEventHandler<HTMLImageElement> = () => {
+    setShowRemovePhoto(true);
+  };
+
+  const onMouseLeaveHandler: MouseEventHandler<HTMLImageElement> = () => {
+    setShowRemovePhoto(false);
+  };
+
+  const addImageHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const images: string[] = [];
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const reader = new FileReader();
+        reader.onload = () => {
+          const imageDataUrl = reader.result as string;
+          images.push(imageDataUrl);
+          setRecipeImages((state) => [...state, imageDataUrl]);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  };
+
   return (
     <form className={styles.form}>
       <div className="form-group">
@@ -18,6 +55,54 @@ const AddRecipe = () => {
           </label>
         </div>
       </div>
+      <label>Recipe Images</label>
+      <div className={styles.imagesContainer}>
+        {recipeImages &&
+          recipeImages.map((rI, index) => (
+            <div
+              className={styles.imageContainer}
+              onMouseOver={() => setShowRemovePhoto(true)}
+              onMouseLeave={() => setShowRemovePhoto(false)}
+              key={index}
+            >
+              <img
+                src={rI}
+                className="img-thumbnail"
+                key={index}
+                onMouseOver={onMouseOverHandler}
+              />
+              <svg
+                type="button"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className={"bi bi-x-square-fill " + styles.buttonOnHover}
+                viewBox="0 0 16 16"
+              >
+                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708" />
+              </svg>
+            </div>
+          ))}
+      </div>
+      <div className="form-group">
+        <div
+          data-mdb-button-init
+          data-mdb-ripple-init
+          className="btn btn-secondary"
+        >
+          <label className="form-label text-white m-1" htmlFor="customFile1">
+            Choose files
+          </label>
+          <input
+            type="file"
+            className="form-control d-none"
+            id="customFile1"
+            multiple
+            onChange={addImageHandler}
+          />
+        </div>
+      </div>
       <div className="form-group">
         <label htmlFor="email" className="form-label">
           Cooking Steps
@@ -33,8 +118,8 @@ const AddRecipe = () => {
       <div className="form-row">
         <div className="form-group col-md-4">
           <label htmlFor="inputState">Difficulty</label>
-          <select id="inputState" className="form-control">
-            <option selected>EAZY</option>
+          <select id="inputState" defaultValue="EAZY" className="form-control">
+            <option>EAZY</option>
             <option>MEDIUM</option>
             <option>HARD</option>
           </select>
@@ -46,16 +131,8 @@ const AddRecipe = () => {
           </div>
         </div>
       </div>
-      <div className="form-group">
-        <div className="form-check">
-          <input className="form-check-input" type="checkbox" id="gridCheck" />
-          <label className="form-check-label" htmlFor="gridCheck">
-            Check me out
-          </label>
-        </div>
-      </div>
       <button type="submit" className="btn btn-primary">
-        Sign in
+        Add Recipe
       </button>
     </form>
   );
